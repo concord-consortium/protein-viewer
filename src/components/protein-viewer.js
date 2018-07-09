2a8bd3dba1484c017ad4ab5c9cf67f4cca5b1a94
Sam Fentress <sfentress@concord.org>
2018-07-09 12:00:44 -0400

    Wrap protein and slider into ProteinViewer component
    This removes all the messy state handling in app.js

---
+++ src/components/protein-viewer.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AminoAcidSlider from './amino-acid-slider'
+import Protein from './protein'
+import InfoBox from './info-box';
+import './protein-viewer.css';
+
+class ProteinViewer extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      selectionStart: 0,
+      showingInfoBox: false,
+      marks: []
+    };
+
+    this.handleUpdateSelectionStart = this.handleUpdateSelectionStart.bind(this);
+    this.handleAminoAcidSliderClick = this.handleAminoAcidSliderClick.bind(this);
+    this.handleMark = this.handleMark.bind(this);
+  }
+
+  handleUpdateSelectionStart(selectionStart) {
+    this.setState({
+      selectionStart: selectionStart
+    });
+  }
+
+  handleAminoAcidSliderClick() {
+    this.setState({
+      showingInfoBox: !this.state.showingInfoBox
+    });
+  }
+
+  handleMark(location) {
+    const existingMarks = this.state.marks;
+    if (existingMarks.indexOf(location) > -1) {
+      existingMarks.splice(existingMarks.indexOf(location), 1);
+    } else {
+      existingMarks.push(location);
+    }
+    this.setState({
+      marks: existingMarks
+    });
+  }
+
+  render() {
+    return (
+      <div className="protein-viewer">
+        <div className="proteins">
+          <Protein
+            width={300}
+            selectionStart={this.state.selectionStart}
+            viewBox="0 0 222 206"
+            svg={this.props.svgImage}
+            marks={this.state.marks.map(loc => loc / this.props.aminoAcids.length)}
+          />
+          { this.props.svgImage2 &&
+            <Protein
+              width={300}
+              selectionStart={this.state.selectionStart}
+              viewBox="0 0 222 206"
+              highlightColor="4, 255, 0"
+              svg={this.props.svgImage2}
+              marks={this.state.marks.map(loc => loc / this.props.aminoAcids.length)}
+            />
+          }
+        </div>
+        <div className="amino-acids">
+          <AminoAcidSlider
+            aminoAcids={this.props.aminoAcids}
+            width={300}
+            selectionStart={this.state.selectionStart}
+            updateSelectionStart={this.handleUpdateSelectionStart}
+            onClick={this.handleAminoAcidSliderClick}
+            marks={this.state.marks}
+          />
+          { this.props.aminoAcids2 &&
+            <AminoAcidSlider
+              aminoAcids={this.props.aminoAcids2}
+              width={300}
+              selectionStart={this.state.selectionStart}
+              updateSelectionStart={this.handleUpdateSelectionStart}
+              onClick={this.handleAminoAcidSliderClick}
+              marks={this.state.marks}
+            />
+          }
+        </div>
+        {this.state.showingInfoBox &&
+          <InfoBox
+            aminoAcids={this.props.aminoAcids}
+            selection={this.state.selectionStart}
+            marks={this.state.marks}
+            onMarkLocation={this.handleMark}
+            // FIXME
+            top={33}
+            left={-83}
+            width={274}
+          />
+        }
+      </div>
+    );
+  }
+}
+
+ProteinViewer.propTypes = {
+  aminoAcids: PropTypes.string,
+  svgImage: PropTypes.string,
+  aminoAcids2: PropTypes.string,
+  svgImage2: PropTypes.string
+};
+
+export default ProteinViewer;
