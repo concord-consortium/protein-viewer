8748702e521c49f0346ab6a602643c4110a9a7da
Eli Kosminsky <ekosmin@mit.edu>
2018-07-25 15:39:26 -0700

    Ejecting and moving folders to configure for npm deployment
    Following instructions here: https://medium.com/@lokhmakov/best-way-to-create-npm-packages-with-create-react-app-b24dd449c354

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ProteinWrapper from './node_modules/components/ProteinWrapper/ProteinWrapper';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ProteinWrapper />, document.getElementById('root'));
 registerServiceWorker();
